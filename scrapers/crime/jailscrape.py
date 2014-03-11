cf77febbc87c7f44018423b39f3539f8ef1d8b40
Thomas Levine <_@thomaslevine.com>
2014-03-11 18:15:12 +0000

    cssselect doesn't prepend '/' to xpaths. darn

--- scrapers/crime/jailscrape.py
+++ scrapers/crime/jailscrape.py
@@ -16,32 +16,29 @@ else:
         pickle.dump(response, fp)
 
 # Open the HTML file and turn it into a BeautifulSoup object for parsing
-html = response.text
-soup = BeautifulSoup(html)
+raw = response.text
+html = lxml.html.fromstring(raw)
 
-# The scrape actually starts here. Let's get the table that contains the results.
-only_table = soup.find('table', attrs={'class': 'resultsTable'})
+# Now we start reading the website. Let's get the table that contains the results.
+only_table = html.cssselect('table.resultsTable')[0]
 
 output_trs = [] # The list that's going to store all of our output rows
 
 # First we need to loop through all the rows in the table
-for tr in only_table.findAll('tr'):
+for tr in only_table.cssselect('tr'):
 
     # And next, we want to get all the cells within each of the rows
-    tds = tr.findAll('td')
+    tds = tr.cssselect('td')
 
     # We'll store all of the values for each given row in a list
     output_tds = []
     for td in tds:
-        output_tds.append(td.text.replace('&nbsp;', '')) # Delete annoying tab character
+        output_tds.append(td.text_content().replace('&nbsp;', '')) # Delete annoying tab character
 
     # And we'll add that list to our broader list of results
     output_trs.append(output_tds)
 
 # Finally, we'll write our results to a file
-
-print(output_trs)
-
 handle = open('out.csv', 'a')
 outfile = csv.writer(handle)
 
